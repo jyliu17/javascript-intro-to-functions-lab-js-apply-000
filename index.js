daa446af06b8262d78704bf1ae4fbb21a0309ed4
Johnson Liu <jyliu17@gmail.com>
2020-06-26 13:46:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function shout(string) {
-  return string.toUpperCase();
+ return string.toUpperCase();
 }
+
 function whisper(string) {
-  return string.toLowerCase();
+ return string.toLowerCase();
 }
 function logShout(string) {
  console.log(string.toUpperCase());
@@ -11,14 +12,9 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  }
- if (string.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
-
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
-}
+  if (string === string.toLowerCase());
+  return `I can't hear you!` ;
+} 
+  if (string === string.toUpperCase()) {
+ return 'YES INDEED!';
+ }   
