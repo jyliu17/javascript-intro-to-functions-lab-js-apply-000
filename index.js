220d386e40c017464784c1d729a0d3c5d6671243
Johnson Liu <jyliu17@gmail.com>
2020-06-26 13:47:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase());
+  if (string === string.toLowerCase()) {
   return `I can't hear you!` ;
 } 
   if (string === string.toUpperCase()) {
  return 'YES INDEED!'
  }   
+}
